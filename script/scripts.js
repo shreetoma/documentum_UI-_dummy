de63a047c797fc65cdc521e4bbc2e46cdde405fc
shreetoma <shreetoma14@gmail.com>
2018-01-21 03:47:31 +0530

    updated

--- script/scripts.js
+++ script/scripts.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp',['ngStorage','ngMessages']);
+var app = angular.module('myApp',['ngStorage','ngMessages','ui.router']);
 app.controller('myController',function($scope,$location, $localStorage){
     $scope.signIn= function(){
         
@@ -11,7 +11,44 @@ app.controller('myController',function($scope,$location, $localStorage){
     }
         
 })
-app.controller('myController2',function($scope, $localStorage, $window){
+app.controller('myController2',function($scope, $localStorage, $window,$http){
    $scope.userName =$localStorage.uname;
-})
+    //alert("hello");
+    
+    function myFnc(){
+        
+        
+    $http({
+        url:'assets/json/data.JSON',
+        method:'GET',
+        headers:{'Content-Type':'application/json'}
+        
+    })
+        
+                .then(function (jsonData) {
+                   console.log(jsonData);
+        $scope.table=jsonData.data;
+                })
+                
 
+    }
+    myFnc();
+    
+    
+})
+app.config(function($stateProvider, $urlRouterProvider){
+     $urlRouterProvider.otherwise('/home-view');
+    $stateProvider
+    .state('home-view',{
+           url:'/home-view',
+        templateUrl: 'view/home-page-view.html'
+        
+    })
+    .state('search-view',{
+           url:'/search-view',
+        templateUrl: 'view/search-view.html'
+        
+    })
+    
+    
+})
